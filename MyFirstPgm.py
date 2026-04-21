6989403290588bbf046a88cd795cd7e76203a0d7
Marakathapriya <marakathapriya13@gmail.com>
2026-04-21 13:28:58 +0530

    Update MyFirstPgm.py
    Passing arguments to add() and sub()

--- MyFirstPgm.py
+++ MyFirstPgm.py
@@ -1,16 +1,16 @@
 def m1():
   print("hello")
-def add():
-  print(10+5)
-def sub():
-  print(10-5)
+def add(a=10,b=20):
+  print(a+b)
+def sub(c,d):
+  print(c-d)
 def mul():
   print(10*5)
 def div():
   print(10/5)
 
 m1()
-add()
-sub()
+add(a,b)
+sub(20,40)
 mul()
 div()
